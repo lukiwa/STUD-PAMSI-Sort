37bd8eaee6afe5f27a4ee22669981f3f69ecd9fc
Lukasz <lukasz.iwan.99@gmail.com>
2020-03-04 22:40:51 +0100

    Added array generator template class

---
+++ prj/include/ArrayGenerator.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <memory>
+
+/**
+ * @brief Generator tablic do sortowania
+ *
+ * @tparam T typ którym wypełniona będzie tablica
+ */
+template <typename T>
+class ArrayGenerator {
+   public:
+    ArrayGenerator() = default;
+    ~ArrayGenerator() = default;
+    /**
+     * @brief Generuje tablice danego typu o danym rozmiarze
+     *
+     * @param size rozmiar tablicy
+     * @return std::tuple<std::size_t, std::unique_ptr<T[]>> para, gdzie 1 wartość oznacza rozmiar
+     * stworzonej tablicy, a 2 jest tablicą
+     */
+    std::tuple<std::size_t, std::unique_ptr<T[]>> GenerateRandomArray(std::size_t size) {
+        srand(time(0));
+        auto array = std::make_unique<T[]>(size);
+        // wypełnienie tablicy losowymi elementami
+        for (std::size_t i = 0; i < size; ++i) {
+            array[i] = std::rand();
+        }
+        return {size, std::move(array)};
+    }
+
+    /**
+     * @brief Generuje częściowo posortowaną tablicę
+     *
+     * @param size Rozmiar tablicy
+     * @param already_sorted Jaki procent tablicy jest juz posortowany
+     * @return std::tuple<std::size_t, std::unique_ptr<T[]>> para, gdzie 1 wartość oznacza rozmiar
+     * stworzonej tablicy, a 2 jest tablicą
+     */
+    std::tuple<std::size_t, std::unique_ptr<T[]>> GeneratePartiallySortedArray(
+        std::size_t size, double already_sorted) {
+        // TODO
+    }
+
+    /**
+     * @brief Generuje tablicę posortowaną od elementu największego do najmniejszego
+     *
+     * @param size rozmiar tablicy
+     * @return std::tuple<std::size_t, std::unique_ptr<T[]>>
+     */
+    std::tuple<std::size_t, std::unique_ptr<T[]>> GenerateReverseSortedArray(std::size_t size) {
+        // TODO
+    }
+};
--- prj/src/main.cpp
+++ prj/src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <memory>
+#include "ArrayGenerator.hpp"
+
+// NOTE testy
+int GetSth(std::tuple<std::size_t, std::unique_ptr<int[]>>& array, int index) {
+    return std::get<1>(array)[index];
+}
 
 int main() {
     std::cout << "Hello Easy C++ project!" << std::endl;
+    ArrayGenerator<int> generator;
+    auto tuple = generator.GenerateRandomArray(100);
+    std::cout << GetSth(tuple, 12) << std::endl;
+    // delete[] std::get<1>(tuple).release();
+    std::get<1>(tuple).reset();
 }
