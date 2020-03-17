eb8bd6fb4398a3efdeedb1b3b9e1e8b3e1942aa3
lukiwa <lukasz.iwan.99@gmail.com>
2020-03-17 10:48:44 +0100

    Added scripts to run all tests on each algorithm + tested merge sort (divided to catalogs)

---
+++ prj/bin/merge.sh
@@ -0,0 +1,47 @@
+./sorting MergeSort 100 10000 0 UP
+./sorting MergeSort 100 50000 0 UP
+./sorting MergeSort 100 100000 0 UP
+./sorting MergeSort 100 500000 0 UP
+./sorting MergeSort 100 1000000 0 UP
+#-------------25%----------------------
+./sorting MergeSort 100 10000 0.25 UP
+./sorting MergeSort 100 50000 0.25 UP
+./sorting MergeSort 100 100000 0.25 UP
+./sorting MergeSort 100 500000 0.25 UP
+./sorting MergeSort 100 1000000 0.25 UP
+#--------------50%-------------------
+./sorting MergeSort 100 10000 0.5 UP
+./sorting MergeSort 100 50000 0.5 UP
+./sorting MergeSort 100 100000 0.5 UP
+./sorting MergeSort 100 500000 0.5 UP
+./sorting MergeSort 100 1000000 0.5 UP
+#--------------75%----------------------
+./sorting MergeSort 100 10000 0.75 UP
+./sorting MergeSort 100 50000 0.75 UP
+./sorting MergeSort 100 100000 0.75 UP
+./sorting MergeSort 100 500000 0.75 UP
+./sorting MergeSort 100 1000000 0.75 UP
+#-----------------95%----------------
+./sorting MergeSort 100 10000 0.95 UP
+./sorting MergeSort 100 50000 0.95 UP
+./sorting MergeSort 100 100000 0.95 UP
+./sorting MergeSort 100 500000 0.95 UP
+./sorting MergeSort 100 1000000 0.95 UP
+#-------------------99%--------------
+./sorting MergeSort 100 10000 0.99 UP
+./sorting MergeSort 100 50000 0.99 UP
+./sorting MergeSort 100 100000 0.99 UP
+./sorting MergeSort 100 500000 0.99 UP
+./sorting MergeSort 100 1000000 0.99 UP
+#-----------------99,7%---------------
+./sorting MergeSort 100 10000 0.997 UP
+./sorting MergeSort 100 50000 0.997 UP
+./sorting MergeSort 100 100000 0.997 UP
+./sorting MergeSort 100 500000 0.997 UP
+./sorting MergeSort 100 1000000 0.997 UP
+#-----------------DOWN------------------
+./sorting MergeSort 100 10000 0 DOWN
+./sorting MergeSort 100 50000 0 DOWN
+./sorting MergeSort 100 100000 0 DOWN
+./sorting MergeSort 100 500000 0 DOWN
+./sorting MergeSort 100 1000000 0 DOWN
