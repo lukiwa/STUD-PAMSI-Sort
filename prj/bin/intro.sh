c63a4ea4048f73090441d02a51cca2ebf1c70a74
lukiwa <lukasz.iwan.99@gmail.com>
2020-03-20 00:15:45 +0100

    Scripts to run tests automatically

--- prj/bin/intro.sh
+++ prj/bin/intro.sh
@@ -1,47 +1,47 @@
-./sorting IntroSort 100 10000 0 UP
-./sorting IntroSort 100 50000 0 UP
-./sorting IntroSort 100 100000 0 UP
-./sorting IntroSort 100 500000 0 UP
-./sorting IntroSort 100 1000000 0 UP
+./sorting.out IntroSort 100 10000 0 UP
+./sorting.out IntroSort 100 50000 0 UP
+./sorting.out IntroSort 100 100000 0 UP
+./sorting.out IntroSort 100 500000 0 UP
+./sorting.out IntroSort 100 1000000 0 UP
 #-------------25%----------------------
-./sorting IntroSort 100 10000 0.25 UP
-./sorting IntroSort 100 50000 0.25 UP
-./sorting IntroSort 100 100000 0.25 UP
-./sorting IntroSort 100 500000 0.25 UP
-./sorting IntroSort 100 1000000 0.25 UP
+./sorting.out IntroSort 100 10000 0.25 UP
+./sorting.out IntroSort 100 50000 0.25 UP
+./sorting.out IntroSort 100 100000 0.25 UP
+./sorting.out IntroSort 100 500000 0.25 UP
+./sorting.out IntroSort 100 1000000 0.25 UP
 #--------------50%-------------------
-./sorting IntroSort 100 10000 0.5 UP
-./sorting IntroSort 100 50000 0.5 UP
-./sorting IntroSort 100 100000 0.5 UP
-./sorting IntroSort 100 500000 0.5 UP
-./sorting IntroSort 100 1000000 0.5 UP
+./sorting.out IntroSort 100 10000 0.5 UP
+./sorting.out IntroSort 100 50000 0.5 UP
+./sorting.out IntroSort 100 100000 0.5 UP
+./sorting.out IntroSort 100 500000 0.5 UP
+./sorting.out IntroSort 100 1000000 0.5 UP
 #--------------75%----------------------
-./sorting IntroSort 100 10000 0.75 UP
-./sorting IntroSort 100 50000 0.75 UP
-./sorting IntroSort 100 100000 0.75 UP
-./sorting IntroSort 100 500000 0.75 UP
-./sorting IntroSort 100 1000000 0.75 UP
+./sorting.out IntroSort 100 10000 0.75 UP
+./sorting.out IntroSort 100 50000 0.75 UP
+./sorting.out IntroSort 100 100000 0.75 UP
+./sorting.out IntroSort 100 500000 0.75 UP
+./sorting.out IntroSort 100 1000000 0.75 UP
 #-----------------95%----------------
-./sorting IntroSort 100 10000 0.95 UP
-./sorting IntroSort 100 50000 0.95 UP
-./sorting IntroSort 100 100000 0.95 UP
-./sorting IntroSort 100 500000 0.95 UP
-./sorting IntroSort 100 1000000 0.95 UP
+./sorting.out IntroSort 100 10000 0.95 UP
+./sorting.out IntroSort 100 50000 0.95 UP
+./sorting.out IntroSort 100 100000 0.95 UP
+./sorting.out IntroSort 100 500000 0.95 UP
+./sorting.out IntroSort 100 1000000 0.95 UP
 #-------------------99%--------------
-./sorting IntroSort 100 10000 0.99 UP
-./sorting IntroSort 100 50000 0.99 UP
-./sorting IntroSort 100 100000 0.99 UP
-./sorting IntroSort 100 500000 0.99 UP
-./sorting IntroSort 100 1000000 0.99 UP
+./sorting.out IntroSort 100 10000 0.99 UP
+./sorting.out IntroSort 100 50000 0.99 UP
+./sorting.out IntroSort 100 100000 0.99 UP
+./sorting.out IntroSort 100 500000 0.99 UP
+./sorting.out IntroSort 100 1000000 0.99 UP
 #-----------------99,7%---------------
-./sorting IntroSort 100 10000 0.997 UP
-./sorting IntroSort 100 50000 0.997 UP
-./sorting IntroSort 100 100000 0.997 UP
-./sorting IntroSort 100 500000 0.997 UP
-./sorting IntroSort 100 1000000 0.997 UP
+./sorting.out IntroSort 100 10000 0.997 UP
+./sorting.out IntroSort 100 50000 0.997 UP
+./sorting.out IntroSort 100 100000 0.997 UP
+./sorting.out IntroSort 100 500000 0.997 UP
+./sorting.out IntroSort 100 1000000 0.997 UP
 #-----------------DOWN------------------
-./sorting IntroSort 100 10000 0 DOWN
-./sorting IntroSort 100 50000 0 DOWN
-./sorting IntroSort 100 100000 0 DOWN
-./sorting IntroSort 100 500000 0 DOWN
-./sorting IntroSort 100 1000000 0 DOWN
+./sorting.out IntroSort 100 10000 0 DOWN
+./sorting.out IntroSort 100 50000 0 DOWN
+./sorting.out IntroSort 100 100000 0 DOWN
+./sorting.out IntroSort 100 500000 0 DOWN
+./sorting.out IntroSort 100 1000000 0 DOWN
--- prj/bin/quick.sh
+++ prj/bin/quick.sh
@@ -1,47 +1,47 @@
-./sorting QuickSort 100 10000 0 UP
-./sorting QuickSort 100 50000 0 UP
-./sorting QuickSort 100 100000 0 UP
-./sorting QuickSort 100 500000 0 UP
-./sorting QuickSort 100 1000000 0 UP
+./sorting.out QuickSort 100 10000 0 UP
+./sorting.out QuickSort 100 50000 0 UP
+./sorting.out QuickSort 100 100000 0 UP
+./sorting.out QuickSort 100 500000 0 UP
+./sorting.out QuickSort 100 1000000 0 UP
 #-------------25%----------------------
-./sorting QuickSort 100 10000 0.25 UP
-./sorting QuickSort 100 50000 0.25 UP
-./sorting QuickSort 100 100000 0.25 UP
-./sorting QuickSort 100 500000 0.25 UP
-./sorting QuickSort 100 1000000 0.25 UP
+./sorting.out QuickSort 100 10000 0.25 UP
+./sorting.out QuickSort 100 50000 0.25 UP
+./sorting.out QuickSort 100 100000 0.25 UP
+./sorting.out QuickSort 100 500000 0.25 UP
+./sorting.out QuickSort 100 1000000 0.25 UP
 #--------------50%-------------------
-./sorting QuickSort 100 10000 0.5 UP
-./sorting QuickSort 100 50000 0.5 UP
-./sorting QuickSort 100 100000 0.5 UP
-./sorting QuickSort 100 500000 0.5 UP
-./sorting QuickSort 100 1000000 0.5 UP
+./sorting.out QuickSort 100 10000 0.5 UP
+./sorting.out QuickSort 100 50000 0.5 UP
+./sorting.out QuickSort 100 100000 0.5 UP
+./sorting.out QuickSort 100 500000 0.5 UP
+./sorting.out QuickSort 100 1000000 0.5 UP
 #--------------75%----------------------
-./sorting QuickSort 100 10000 0.75 UP
-./sorting QuickSort 100 50000 0.75 UP
-./sorting QuickSort 100 100000 0.75 UP
-./sorting QuickSort 100 500000 0.75 UP
-./sorting QuickSort 100 1000000 0.75 UP
+./sorting.out QuickSort 100 10000 0.75 UP
+./sorting.out QuickSort 100 50000 0.75 UP
+./sorting.out QuickSort 100 100000 0.75 UP
+./sorting.out QuickSort 100 500000 0.75 UP
+./sorting.out QuickSort 100 1000000 0.75 UP
 #-----------------95%----------------
-./sorting QuickSort 100 10000 0.95 UP
-./sorting QuickSort 100 50000 0.95 UP
-./sorting QuickSort 100 100000 0.95 UP
-./sorting QuickSort 100 500000 0.95 UP
-./sorting QuickSort 100 1000000 0.95 UP
+./sorting.out QuickSort 100 10000 0.95 UP
+./sorting.out QuickSort 100 50000 0.95 UP
+./sorting.out QuickSort 100 100000 0.95 UP
+./sorting.out QuickSort 100 500000 0.95 UP
+./sorting.out QuickSort 100 1000000 0.95 UP
 #-------------------99%--------------
-./sorting QuickSort 100 10000 0.99 UP
-./sorting QuickSort 100 50000 0.99 UP
-./sorting QuickSort 100 100000 0.99 UP
-./sorting QuickSort 100 500000 0.99 UP
-./sorting QuickSort 100 1000000 0.99 UP
+./sorting.out QuickSort 100 10000 0.99 UP
+./sorting.out QuickSort 100 50000 0.99 UP
+./sorting.out QuickSort 100 100000 0.99 UP
+./sorting.out QuickSort 100 500000 0.99 UP
+./sorting.out QuickSort 100 1000000 0.99 UP
 #-----------------99,7%---------------
-./sorting QuickSort 100 10000 0.997 UP
-./sorting QuickSort 100 50000 0.997 UP
-./sorting QuickSort 100 100000 0.997 UP
-./sorting QuickSort 100 500000 0.997 UP
-./sorting QuickSort 100 1000000 0.997 UP
+./sorting.out QuickSort 100 10000 0.997 UP
+./sorting.out QuickSort 100 50000 0.997 UP
+./sorting.out QuickSort 100 100000 0.997 UP
+./sorting.out QuickSort 100 500000 0.997 UP
+./sorting.out QuickSort 100 1000000 0.997 UP
 #-----------------DOWN------------------
-./sorting QuickSort 100 10000 0 DOWN
-./sorting QuickSort 100 50000 0 DOWN
-./sorting QuickSort 100 100000 0 DOWN
-./sorting QuickSort 100 500000 0 DOWN
-./sorting QuickSort 100 1000000 0 DOWN
+./sorting.out QuickSort 100 10000 0 DOWN
+./sorting.out QuickSort 100 50000 0 DOWN
+./sorting.out QuickSort 100 100000 0 DOWN
+./sorting.out QuickSort 100 500000 0 DOWN
+./sorting.out QuickSort 100 1000000 0 DOWN
